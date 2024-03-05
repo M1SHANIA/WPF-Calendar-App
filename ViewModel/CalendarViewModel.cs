afa34b83a9a78dc9afd2881fa65b8ab0f8cc4454
M1SHANYA <mihaonipchenko@gmail.com>
2024-03-05 17:56:24 +0100

    repo init

---
+++ ViewModel/CalendarViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calendar.ViewModel
+{
+    public class CalendarViewModel : INotifyPropertyChanged
+    {
+        private DateTime _currentDate;
+
+        public DateTime CurrentDate
+        {
+            get { return _currentDate; }
+            set
+            {
+                _currentDate = value;
+                OnPropertyChanged("CurrentDate");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public CalendarViewModel()
+        {
+            // Установка начального значения для текущей даты (сегодняшняя дата)
+            CurrentDate = DateTime.Today;
+        }
+    }
+}
