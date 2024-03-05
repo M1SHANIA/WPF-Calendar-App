7f415dbc0b20c72153dde5dff58cb8701ff9390c
M1SHANYA <M1SHANYA@DESKTOP-JDKC543>
2024-03-05 17:45:33 +0100

    Добавьте файлы проекта.

---
+++ Model/EventModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calendar.Model
+{
+    public class EventModel
+    {
+        public int Id { get; set; } 
+        public string Title { get; set; } 
+        public DateTime StartDate { get; set; } 
+        public DateTime EndDate { get; set; } 
+        public string Description { get; set; } 
+        public List<TaskModel> Tasks { get; set; } 
+    }
+
+}
